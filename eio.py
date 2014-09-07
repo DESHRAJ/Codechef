3b47c575d95c9f643069ff90ec6dbd7c4a188932
deshraj <deshrajdry@gmail.com>
2014-09-07 21:24:46 +0530

    new codes added

--- eio.py
+++ eio.py
@@ -1,14 +1,10 @@
-n, k = raw_input().split(" ")
+n, k = input().split(" ")
 n = int(n)
 k = int(k)
 i,j=0,0
-t = []
 for i in range(0,n):
-	a = raw_input()
+	a = input()
 	a = int(a)
-	t.append(a)
-if (k>0):
-    for x in t:
-        if(x%k==0):
-            j+=1
+	if(x%k == 0):
+		j+=1
 print j
