6324e5ba32a6fb5c290d3a21ba0238f953e5392e
deshraj <deshrajdry@gmail.com>
2014-09-13 23:36:05 +0530

    13 sept 2014

--- prime_generator.py
+++ prime_generator.py
@@ -1,14 +1,14 @@
 import math 
 
 def primecheck(a):
+	a = int(a)
 	if (a==1):
 		return 0
-	if (a==2):
+	elif (a==2):
 		return a
 	elif (a>2):
-		i=2
 		flag=0
-		for i in range(2,int(math.sqrt(a))):
+		for i in range(2,int(math.sqrt(a)+1)):
 			if(a%i==0):
 				flag=1
 				break
